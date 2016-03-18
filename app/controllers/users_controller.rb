bfa5c1f350edbae7f1a67b5ccc4eb748e569ea58
jmhol9 <jordanholliday@gmail.com>
2016-03-18 05:46:23 -0400

    add picks on Users#show, links to user detail pages on Users#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,41 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    past_rounds = Round.where("picks_end < ?", DateTime.now)
+                        .pluck(:id)
+                        .join(",")
+    # @user_picks = [1]
+    @user_picks = ActiveRecord::Base.connection.select_all(<<-SQL
+      SELECT
+        rounds.id,
+        rounds.name AS round_name,
+        teams.name,
+        teams.seed,
+        picks.points,
+        picks.multiplier,
+        (picks.points *
+          (CASE teams.seed WHEN 1 THEN 1.5 ELSE teams.seed END) *
+          (CASE team_round_results.win WHEN true THEN 1 ELSE 0 END) *
+          picks.multiplier) as payout,
+        team_round_results.win AS result
+      FROM
+        users
+      LEFT JOIN
+        picks ON picks.user_id = users.id
+      JOIN
+        teams ON teams.id = picks.team_id
+      LEFT JOIN
+        team_round_results ON (
+        team_round_results.round_id = picks.round_id AND
+        team_round_results.team_id = picks.team_id
+        )
+      JOIN
+        rounds ON rounds.id = picks.round_id
+      WHERE
+        users.id  = #{@user.id} AND
+        picks.round_id IN (#{past_rounds})
+      SQL
+      ).sort_by {|pick| pick["id"].to_i}
   end
 
   def scoreboard
@@ -36,7 +71,7 @@ class UsersController < ApplicationController
   end
 
   def scores_by_user_by_round
-    ActiveRecord::Base.connection.select_all( <<-SQL
+    ActiveRecord::Base.connection.select_all(<<-SQL
       SELECT
         users.team_name,
         users.name,

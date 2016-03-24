4845f2d8fc1763cc5f922bc0633ec8c2d4363fe6
jmhol9 <jordanholliday@gmail.com>
2016-03-24 13:26:38 -0400

    Print user names on scoreboard

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -2,6 +2,6 @@ class RoundsController < ApplicationController
   before_action :redirect_unless_admin
 
   def index
-    @rounds = Round.where("picks_end < ?", DateTime.now + 24.hours).order(id: :desc).limit(2)
+    @rounds = Round.where("picks_end < ?", DateTime.now + 24.hours).order(id: :desc).limit(2).reverse
   end
 end
